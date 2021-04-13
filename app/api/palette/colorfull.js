db40bc4440b8b3244110c47da772f84a29f65d42
Stefan Joseph <stef5671@gmail.com>
2021-04-13 10:38:54 +0530

    stat page changes

--- app/api/palette/colorfull.js
+++ app/api/palette/colorfull.js
@@ -5,11 +5,12 @@ import teal from '@material-ui/core/colors/teal';
 import lightGreen from '@material-ui/core/colors/lightGreen';
 import lightBlue from '@material-ui/core/colors/lightBlue';
 import indigo from '@material-ui/core/colors/indigo';
+import blue from '@material-ui/core/colors/blue';
 const colorfull = [
   pink[400],
   purple[500],
-  lightBlue[500],
-  teal[500],
+  lightBlue[800],
+  blue[300],
   orange[600],
   lightGreen[600],
   indigo.A200,
