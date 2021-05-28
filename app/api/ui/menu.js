13f4e3486e79e91668a0a649e25cc5871465b58e
Dushmantha <dushmantha@plano.co>
2021-05-28 22:07:16 +0530

    menu items updated and ui fixes

--- app/api/ui/menu.js
+++ app/api/ui/menu.js
@@ -1,29 +1,4 @@
-module.exports = [
-  // {
-  //   key: 'home',
-  //   name: 'Home',
-  //   icon: 'ios-home-outline',
-  //   child: [
-  //     // {
-  //     //   key: 'landing_page',
-  //     //   name: 'Landing Page',
-  //     //   title: true,
-  //     // },
-  //     {
-  //       key: 'app',
-  //       name: 'App',
-  //       link: '/app',
-  //       // badge: 'Hot',
-  //       icon: 'ios-globe-outline',
-  //     },
-  //     {
-  //       key: 'stat',
-  //       name: 'Stat',
-  //       link: '/app/stat',
-  //       icon: 'ios-desktop-outline',
-  //     },
-  //   ],
-  // },
+module.exports = [  
   {
     key: 'optimizer',
     name: 'Optimizer',
@@ -62,14 +37,14 @@ module.exports = [
       {
         key: 'buy-oil',
         name: 'Buy OIL',
-        link: 'https://farm.crudeoil.finance/',
+        link: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xb1b17dff66d75b29d34f0bf8622c406d8219b507',
         icon: 'md-clipboard',
         externalLink: true
       },
       {
         key: 'buy-diesel',
         name: 'Buy DIESEL',
-        link: 'https://farm.crudeoil.finance/',
+        link: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xe1ea2e1907d93f154234ce3b5a7418faf175fe11',
         icon: 'md-clipboard',
         externalLink: true
       },
@@ -95,126 +70,5 @@ module.exports = [
         externalLink: true
       },
     ],
-  },
-  // {
-  //   key: 'shop',
-  //   name: 'Shops',
-  //   icon: 'md-appstore',
-  //   child: [
-  //     {
-  //       key: 'manage-shops',
-  //       name: 'Manage',
-  //       title: true,
-  //     },
-  //     {
-  //       key: 'ListShops',
-  //       name: 'List Shops',
-  //       link: '/app/shops',
-  //       icon: 'ios-contact-outline',
-  //     },
-  //     {
-  //       key: 'email',
-  //       name: 'Email',
-  //       link: '/app/pages/email',
-  //       badge: '2',
-  //       icon: 'ios-mail-outline',
-  //     },
-  //     {
-  //       key: 'social_apps',
-  //       name: 'Social',
-  //       title: true,
-  //     },
-  //     {
-  //       key: 'timeline',
-  //       name: 'Timeline',
-  //       link: '/app/pages/timeline',
-  //       icon: 'ios-people-outline',
-  //     },
-  //     {
-  //       key: 'chat',
-  //       name: 'Chat',
-  //       link: '/app/pages/chat',
-  //       badge: '4',
-  //       icon: 'ios-chatbubbles-outline',
-  //     },
-  //     {
-  //       key: 'ecommerce_app',
-  //       name: 'Ecommerce',
-  //       title: true,
-  //     },
-  //     {
-  //       key: 'Itemlist',
-  //       name: 'Product Catalogues',
-  //       link: '/app/pages/ecommerce',
-  //       icon: 'ios-apps-outline',
-  //     },
-  //   ],
-  // },
-  // {
-  //   key: 'Products',
-  //   name: 'Products',
-  //   icon: 'md-aperture',
-  //   child: [
-  //     {
-  //       key: 'ProductCategory',
-  //       name: 'Product List',
-  //       title: true,
-  //     },
-  //     {
-  //       key: 'AllProductTypes',
-  //       name: 'All Product Types',
-  //       link: '/app/product/types',
-  //       icon: 'ios-person-outline',
-  //     },
-  //   ],
-  // },
-  // {
-  //   key: 'Delivery',
-  //   name: 'Delivery Location',
-  //   icon: 'md-locate',
-  //   child: [
-  //     {
-  //       key: 'LocationsToDelivery',
-  //       name: 'Locations to deliver',
-  //       title: true,
-  //     },
-  //     {
-  //       key: 'AllProductTypes',
-  //       name: 'All Product Types',
-  //       link: '/app/delivery',
-  //       icon: 'ios-car',
-  //     },
-  //   ],
-  // },
-  // {
-  //   key: 'Product',
-  //   name: 'Product',
-  //   icon: 'ios-cog',
-  //   child: [
-  //     {
-  //       key: 'Products',
-  //       name: 'Products',
-  //       link: '/app/products',
-  //       icon: 'ios-cog',
-  //     },
-  //   ],
-  // },
-  // {
-  //   key: 'Category',
-  //   name: 'Category',
-  //   icon: 'ios-cube',
-  //   child: [
-  //     {
-  //       key: 'GoodsCategory',
-  //       name: 'Goods Category',
-  //       title: true,
-  //     },
-  //     {
-  //       key: 'ListCategory',
-  //       name: 'Category',
-  //       link: '/app/category',
-  //       icon: 'ios-cube',
-  //     },
-  //   ],
-  // },
+  }
 ];
